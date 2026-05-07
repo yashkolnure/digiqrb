c73014c57d42847096994ea46103e5d98970038d
unknown <yashkolnure58@gmail.com>
2026-05-07 18:41:40 +0530

    Initial commit

--- src/server.js
+++ src/server.js
@@ -12,7 +12,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
